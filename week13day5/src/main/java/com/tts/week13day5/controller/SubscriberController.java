478e75fae8c45c3e00e01fa82b8ae81aa67e7895
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-14 14:12:47 -0400

    first commit

--- week13day5/src/main/java/com/tts/week13day5/controller/SubscriberController.java
+++ week13day5/src/main/java/com/tts/week13day5/controller/SubscriberController.java
@@ -5,7 +5,9 @@ import com.tts.week13day5.model.Subscriber;
 import com.tts.week13day5.repository.SubscriberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,10 +16,22 @@ public class SubscriberController {
 
     @Autowired
     private SubscriberRepository subscriberRepository;
-
-    @GetMapping(value = "/")
+    @GetMapping(value= "/")
     public String index(Subscriber subscriber) {
-
         return "subscriber/index";
     }
+
+
+    private Subscriber subscriber;
+    @PostMapping(value = "/")
+    public String addNewSubscriber(Subscriber subscriber, Model model) {
+        subscriberRepository.save(new Subscriber(subscriber.getFirstName(),
+                subscriber.getLastName(), subscriber.getUserName(), subscriber.getSignedUp()));
+        model.addAttribute("firstName", subscriber.getFirstName());
+        model.addAttribute("lastName", subscriber.getLastName());
+        model.addAttribute("userName", subscriber.getUserName());
+        return "subscriber/result";
+    }
+
+
 }
