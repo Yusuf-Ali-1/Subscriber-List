6d4b2b9ac03220b8c60a666a80886a68533b1be4
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-14 13:50:28 -0400

    first commit

--- week13day5/src/main/java/com/tts/week13day5/controller/SubscriberController.java
+++ week13day5/src/main/java/com/tts/week13day5/controller/SubscriberController.java
@@ -2,6 +2,8 @@ package com.tts.week13day5.controller;
 
 
 import com.tts.week13day5.model.Subscriber;
+import com.tts.week13day5.repository.SubscriberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SubscriberController {
+
+    @Autowired
+    private SubscriberRepository subscriberRepository;
 
     @GetMapping(value = "/")
     public String index(Subscriber subscriber) {
